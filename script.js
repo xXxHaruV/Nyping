494a1d129f6c70962f6cd1219132392186f2780d
Satsuki Haru <haru.omoto.reimei01@gmail.com>
2025-06-24 17:13:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const words = [
-  "apple", "banana", "computer", "javascript", "keyboard",
-  "penguin", "universe", "giraffe", "program", "language", "cry", "uniuniuni", "aaaaaa"
+  "Hayashidesuka?", "cry", "uniuniuni", "aaaaaa"
 ];
 
 let currentWord = "";
